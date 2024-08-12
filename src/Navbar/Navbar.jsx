9747e56818c451085cdad79f5b3cf5a038788c4e
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-12 23:00:15 +0300

    Add icons to the navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -4,20 +4,32 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="flex justify-between">
-      <h1 className="font-bold">Gamers Hub</h1>
+      <h1 className="font-bold text-2xl">
+        <Link to={"/"}> Gamers Hub</Link>
+      </h1>
 
       <ul className="flex">
-        <li className="mr-5 font-bold">
-          <Link to={"/competitions"}>Competitions</Link>
+        <li className="mr-6">
+          <Link to={"/tournaments"}>
+            <i class="las la-trophy mr-2"></i>Tournaments
+          </Link>
         </li>
-        <li className="mr-5 font-bold">
-          <Link to={"/clans"}>Clans</Link>
+        <li className="mr-6">
+          <Link to={"/clans"}>
+            <i class="las la-skull-crossbones mr-2"></i>
+            Clans
+          </Link>
         </li>
-        <li className="mr-5 font-bold">
-          <Link to={"/players"}>Players</Link>
+        <li className="mr-6">
+          <Link to={"/players"}>
+            <i class="las la-user mr-2"></i>
+            Players
+          </Link>
         </li>
-        <li className="font-bold">
-          <Link to={"/streamers"}>Streamers</Link>
+        <li>
+          <Link to={"/streamers"}>
+            <i class="lab la-twitch mr-2"></i> Streamers
+          </Link>
         </li>
       </ul>
     </div>
