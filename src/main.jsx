57824f5adde22eb8bf86c2abf5e8c68a4461e4e3
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-12 22:59:11 +0300

    Add line icons dependency

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./Styles/input.css";
+import "line-awesome/dist/line-awesome/css/line-awesome.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
