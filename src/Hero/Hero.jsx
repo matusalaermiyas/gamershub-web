eaae6e3b95887fe6ef60ffac7fd517a674c419d1
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-12 11:15:28 +0300

    Add hero section

---
+++ src/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <p className="text-4xl">A Place Where Peoples Organize Events</p>
+    </div>
+  );
+}
+
+export default Hero;
