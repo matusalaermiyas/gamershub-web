ad2bf089758d553d371b51d3ab691603bbf6051c
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-12 11:27:15 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Competitions from "./Competitions/Competitions";
 import Home from "./Home/Home";
 
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/competitions",
+    element: <Competitions />,
+  },
+]);
+
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
