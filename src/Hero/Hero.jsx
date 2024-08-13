eccb520a8cb585288a87e6c84f256c9e216d04c1
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-13 10:46:06 +0300

    Change the layout

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <div>
-      <p className="text-4xl">A Place Where Peoples Organize Events</p>
+    <div className="flex flex-col">
+      <p className="text-4xl self-center">Welcome to Gamers Hub</p>
+      <p className="text-4xl mt-5 self-center">
+        PUBG gaming organization place
+      </p>
+      <p className="text-4xl mt-5 self-center">for Ethiopians</p>
+      <Link
+        to={"/tournaments"}
+        className="bg-white text-gray-800 p-5 rounded-full mt-3 w-max self-center"
+      >
+        View Tournaments
+      </Link>
     </div>
   );
 }
