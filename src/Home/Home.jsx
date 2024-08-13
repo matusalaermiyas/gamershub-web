444b8ce6cb47a9c5f4ad4c75916678f23f3b6bde
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-13 10:46:24 +0300

    Handle overflow

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,10 +4,12 @@ import Hero from "../Hero/Hero";
 
 function Home() {
   return (
-    <div className="w-screen h-screen   bg-[url('/assets/bg-1.jpg')] bg-cover bg-center  text-white p-10 flex flex-col font-custom">
-      <Navbar />
-      <div className="h-full w-full flex flex-col justify-center items-center">
-        <Hero />
+    <div className="w-screen h-screen  bg-[url('/assets/bg-1.jpg')] bg-cover bg-center text-white font-custom">
+      <div className="w-screen h-screen p-10 flex flex-col bg-black bg-opacity-20 ">
+        <Navbar />
+        <div className="h-full w-full flex flex-col justify-center items-center">
+          <Hero />
+        </div>
       </div>
     </div>
   );
