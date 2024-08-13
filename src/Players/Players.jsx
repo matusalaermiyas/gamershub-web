4913d713f65f3213774ef8c54a5ea73fdbf15896
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-13 10:43:52 +0300

    Add Players screen

--- src/Players/Players.jsx
+++ src/Players/Players.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 function Players() {
-  return <div>Players</div>;
+  return (
+    <div className="w-screen h-screen text-white font-custom bg-light-dark">
+      <div className="w-screen pt-10 px-52 flex flex-col bg-light-dark ">
+        <Navbar />
+
+        <div className="mt-10"></div>
+
+        <h1>Players</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Players;
