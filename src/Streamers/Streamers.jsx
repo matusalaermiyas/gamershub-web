7449653dfe42f053bc11d5c2cd2a39f241cd67df
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-13 10:43:35 +0300

    Add Streamers screen

--- src/Streamers/Streamers.jsx
+++ src/Streamers/Streamers.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 function Streamers() {
-  return <div>Streamers</div>;
+  return (
+    <div className="w-screen h-screen text-white font-custom bg-light-dark">
+      <div className="w-screen pt-10 px-52 flex flex-col bg-light-dark ">
+        <Navbar />
+
+        <div className="mt-10"></div>
+
+        <h1>Streamers</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Streamers;
