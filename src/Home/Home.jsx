15f0ee099bb051882d65e307f73d9325dbeb5bfe
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-14 20:40:20 +0300

    Change home style

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,7 +5,7 @@ import Hero from "../Hero/Hero";
 function Home() {
   return (
     <div className="w-screen h-screen  bg-[url('/assets/bg-1.jpg')] bg-cover bg-center text-white font-custom">
-      <div className="w-screen h-screen p-10 flex flex-col bg-black bg-opacity-20 ">
+      <div className="w-screen h-screen p-10 flex flex-col bg-black bg-opacity-30 ">
         <Navbar />
         <div className="h-full w-full flex flex-col justify-center items-center">
           <Hero />
