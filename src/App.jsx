b3b902b5b0aca8d0ba2895481d0aa7ddee83e9b7
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-14 20:39:25 +0300

    Disable routes temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,49 @@ import Streamers from "./Streamers/Streamers";
 import Players from "./Players/Players";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./Register/Register";
+import { MantineProvider } from "@mantine/core";
+import { Toaster } from "react-hot-toast";
+import RegisteredClans from "./RegisteredClans/RegisteredClans";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Register />,
   },
   {
     path: "/tournaments",
     element: <Competitions />,
   },
+  // {
+  //   path: "/clans",
+  //   element: <Clans />,
+  // },
+  // {
+  //   path: "/streamers",
+  //   element: <Streamers />,
+  // },
+  // {
+  //   path: "/players",
+  //   element: <Players />,
+  // },
+  // {
+  //   path: "/register",
+  //   element: <Register />,
+  // },
   {
-    path: "/clans",
-    element: <Clans />,
-  },
-  {
-    path: "/streamers",
-    element: <Streamers />,
-  },
-  {
-    path: "/players",
-    element: <Players />,
+    path: "/view-clans",
+    element: <RegisteredClans />,
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <MantineProvider>
+      <Toaster />
+      <RouterProvider router={router} />
+    </MantineProvider>
+  );
 }
 
 export default App;
