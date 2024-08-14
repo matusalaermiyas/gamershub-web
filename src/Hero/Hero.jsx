c4866ea118a40167187dbda97954d30b0f7d5f49
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-14 20:40:08 +0300

    Change hero style

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -4,11 +4,18 @@ import { Link } from "react-router-dom";
 function Hero() {
   return (
     <div className="flex flex-col">
-      <p className="text-4xl self-center">Welcome to Gamers Hub</p>
+      <p className="text-6xl self-center font-bold">Welcome to Gamers Hub</p>
       <p className="text-4xl mt-5 self-center">
-        PUBG gaming organization place
+        the ultimate PUBG gaming community for Ethiopians
       </p>
-      <p className="text-4xl mt-5 self-center">for Ethiopians</p>
+
+      <p className="text-4xl self-center">
+        Join us and be part of a thriving organization where gamers come
+        together
+      </p>
+      <p className="text-4xl  self-center">
+        to compete, connect, and conquer the battlefield.
+      </p>
       <Link
         to={"/tournaments"}
         className="bg-white text-gray-800 p-5 rounded-full mt-3 w-max self-center"
