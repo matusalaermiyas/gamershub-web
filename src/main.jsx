1b425948d4ff31f7d718fe35dd62f07dd29bcb62
MatusalaErmiyas <matusala.dev@gmail.com>
2024-08-14 20:39:42 +0300

    Add mantine css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App.jsx";
 import "./Styles/input.css";
 import "line-awesome/dist/line-awesome/css/line-awesome.css";
 
+import "@mantine/core/styles.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
